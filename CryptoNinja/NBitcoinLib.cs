cda6a17cfa5f3fefd149cae5c12bd0c674b378bf
Mark Sagal <mark@sagal.biz>
2019-12-01 20:49:14 +0800

    Created NBitcoinLib, NBitcoinLibTests, Helpers/Hash

---
+++ CryptoNinja/NBitcoinLib.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using NBitcoin;
+
+namespace CryptoNinja
+{
+    using Helpers;
+
+    /// <summary>
+    /// NBitcoin Library
+    /// </summary>
+    public class NBitcoinLib
+    {
+        public static Key FromBrainwalletSeed(string seed, bool isCompressedKey = false, int sha256 = 1)
+        {
+            if (sha256 < 1)
+                throw new InvalidOperationException();
+            byte[] data = sha256 == 1 ? Hash.ComputeSha256(seed) : Hash.GetSha256Hash(seed, sha256);
+
+            return new Key(data, fCompressedIn: isCompressedKey);
+        }
+    }
+}
